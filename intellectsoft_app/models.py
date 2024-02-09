ddbbe8c9aea64bc615b36b785bcde55a9b46a917
Coffe Cat <nikitatyshchenkoofficial@gmail.com>
2024-02-09 23:38:18 +0200

    Add models, set up settings, add models to admin site

---
+++ intellectsoft_app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.phone}"
+
+
+class Request(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Rejected', 'Rejected'),
+    ]
+
+    body = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    processed_by = models.ForeignKey('Operator', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Request {self.id} Status {self.status} Processed by {self.processed_by}"
+
+
+class Operator(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
